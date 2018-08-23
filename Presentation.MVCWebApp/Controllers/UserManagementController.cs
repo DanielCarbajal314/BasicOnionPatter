f0bbea7de7c94c446832e2ebec1b94516defe289
Daniel.Carbajal314 <Daniel.carbajal@pucp.edu.pe>
2018-08-22 20:58:42 -0500

    MVC presentation

---
+++ Presentation.MVCWebApp/Controllers/UserManagementController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.MVCWebApp.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Presentation.MVCWebApp/Controllers/apiControllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Services.Implementation.SqlPesistance;
+using Services.Interfaces;
+using Services.Interfaces.Requests;
+using Services.Interfaces.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Presentation.WebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        IUserManager _userManager;
+
+        public UserController()
+        {
+            this._userManager = new UserManagerSQL();
+        }
+
+        [HttpPost]
+        public RegisteredUser Create(CreateUser createUserData)
+        {
+            return this._userManager.Create(createUserData);
+        }
+
+        [HttpDelete]
+        public void Delete(int userId)
+        {
+            this._userManager.Delete(userId);
+        }
+
+        [HttpGet]
+        public List<RegisteredUser> List()
+        {
+            return this._userManager.List();
+        }
+
+        [HttpGet]
+        public RegisteredUser Search(int userId)
+        {
+            return this._userManager.Search(userId);
+        }
+
+        [HttpPut]
+        public RegisteredUser Update(UpdateUser updateUserData)
+        {
+            return this._userManager.Update(updateUserData);
+        }
+    }
+}
