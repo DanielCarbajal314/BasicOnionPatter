1312288be9f4dcb6f4367e725dc3424efaa889a7
Daniel.Carbajal314 <Daniel.carbajal@pucp.edu.pe>
2018-08-27 21:41:40 -0500

    Adding DBInitializer

---
+++ Infraestructure.SQLServerRepository/DocumentDB/SeedData/DocumentManagementDbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.SQLServerRepository.DocumentDB.SeedData
+{
+    public class DocumentManagementDbInitializer:DropCreateDatabaseAlways<DocumentContext>
+    {
+        protected override void Seed(DocumentContext context)
+        {
+            context.Users.Add(new User()
+            {
+                Login = "SystemAdmin",
+                Email = "SystemAdmin@company.com",
+                LastNames = "Admin",
+                Names = "System",
+                Password = "123@123@"
+            });
+            base.Seed(context);
+        }
+    }
+}
--- Infraestructure.TestConsoleRunner/Program.cs
+++ Infraestructure.TestConsoleRunner/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Domain.Entities;
 using Infraestructure.SQLServerRepository.DocumentDB;
+using Infraestructure.SQLServerRepository.DocumentDB.SeedData;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ namespace Infraestructure.TestConsoleRunner
     {
         static void Main(string[] args)
         {
+            Database.SetInitializer<DocumentContext>(new DocumentManagementDbInitializer());
             using (DocumentContext documentDb = new DocumentContext())
             {
                 documentDb.Clients.ToList().ForEach(x =>
