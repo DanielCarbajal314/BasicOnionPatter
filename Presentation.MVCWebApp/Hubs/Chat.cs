d3ffcc89d36fed57b9a91d8e0c5a00afe96ec90c
Daniel.Carbajal314 <Daniel.carbajal@pucp.edu.pe>
2018-09-10 20:10:24 -0500

    SignalR

---
+++ Presentation.MVCWebApp/Hubs/Chat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Presentation.MVCWebApp.Hubs
+{
+    public class Chat : Hub
+    {
+        public void PushMessage(UserMessage userMessage)
+        {
+            Clients.Others.NewMessage(userMessage);
+        }
+
+        public void suscribe()
+        {
+            
+        }
+    }
+
+    public class UserMessage {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
